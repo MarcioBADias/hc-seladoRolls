3150f79473a8a59c1a6e2f3d82c23d230078ebb9
marciobadias <marcio.b.a.dias@gmail.com>
2024-06-11 23:14:44 -0300

    ✅ Fazendo as imagens serem renderizadas conforme sorteado

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [resultado, setResultado] = useState('')
+  const [resultado, setResultado] = useState([])
 
   const rolar = () => {
     let array = []
-    array.push(gerarNumeroAleatorio(1, 17))
-    array.push(gerarNumeroAleatorio(1, 17))
-    array.push(gerarNumeroAleatorio(18, 32))
-    array.push(gerarNumeroAleatorio(33, 46))
-    array.push(gerarNumeroFinal())
+    array.push(formatarNumero(gerarNumeroAleatorio(1, 17)))
+    array.push(formatarNumero(gerarNumeroAleatorio(1, 17)))
+    array.push(formatarNumero(gerarNumeroAleatorio(18, 32)))
+    array.push(formatarNumero(gerarNumeroAleatorio(33, 46)))
+    array.push(formatarNumero(gerarNumeroFinal()))
 
     setResultado(array)
     console.log(resultado)
@@ -19,15 +19,19 @@ const App = () => {
     return Math.floor(Math.random() * (max - min + 1)) + min
   }
 
+  const formatarNumero = (numero) => {
+    return numero.toString().padStart(3, '0')
+  }
+
   const gerarNumeroFinal = () => {
     const opcoes = [
       ...gerarRange(18, 32),
       ...gerarRange(47, 70),
-      '37b',
-      '43b',
-      '47b',
-      '51b',
-      '53b',
+      '037b',
+      '043b',
+      '047b',
+      '051b',
+      '053b',
     ]
     const index = Math.floor(Math.random() * opcoes.length)
     return opcoes[index]
@@ -43,6 +47,9 @@ const App = () => {
   return (
     <>
       <button onClick={() => rolar()}>Abrir Booster</button>
+      {resultado.map((token) => (
+        <img key={token} src={`dwx${token}tk.png`} alt="Token" />
+      ))}
       <p>{resultado}</p>
     </>
   )
